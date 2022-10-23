70f9ac357f4efa85175e54b91add7a0cda0186ec
qiaosong <qiaosong@ualberta.ca>
2022-10-23 01:27:05 -0600

    US 02.07.01 has been finished. Changes in the database are now reflected on the listview in real time.

--- app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
+++ app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
@@ -5,8 +5,11 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -17,6 +20,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +49,14 @@ public class MainActivity extends AppCompatActivity {
                         Log.w(TAG, "Error adding document", e);
                     }
                 });
+
+        Recipe = findViewById(R.id.Recipe);
+        Recipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent RecipeIntent = new Intent(MainActivity.this, RecipeActivity.class);
+                startActivity(RecipeIntent);
+            }
+        });
     }
 }
