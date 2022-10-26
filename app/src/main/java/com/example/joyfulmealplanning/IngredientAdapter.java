a0de8cf59a4f5918380eb61ca34bb436876e54e9
Xiangxu Meng <xiangxu@ualberta.ca>
2022-10-26 06:51:26 -0600

    US 01.05.01 Finish
    Finish the US 01.05.01 (View a list of ingredients). Fix issues that does not match between classes.

--- app/src/main/java/com/example/joyfulmealplanning/IngredientAdapter.java
+++ app/src/main/java/com/example/joyfulmealplanning/IngredientAdapter.java
@@ -39,27 +39,24 @@ public class IngredientAdapter extends ArrayAdapter<Ingredients> {
             view = LayoutInflater.from(context).inflate(R.layout.content, parent,false);
         }
 
-        //CardView cardView;
-        //CalendarView calendarView;
+        Ingredients ingredient = ingredients.get(position);
         TextView desText, locationText, CategoryText, AmountText, UnitText, DateText;
 
-        //cardView = view.findViewById(R.id.cardView);
-        //calendarView = view.findViewById(R.id.calendarView2);
-        desText = view.findViewById(R.id.textView4);
-        locationText = view.findViewById(R.id.textView5);
-        CategoryText = view.findViewById(R.id.textView6);
-        AmountText = view.findViewById(R.id.textView7);
-        UnitText = view.findViewById(R.id.textView8);
-        DateText = view.findViewById(R.id.textView9);
+        desText = view.findViewById(R.id.description_text);
+        locationText = view.findViewById(R.id.location_text);
+        CategoryText = view.findViewById(R.id.category_text);
+        AmountText = view.findViewById(R.id.amount_text);
+        UnitText = view.findViewById(R.id.unit_text);
+        DateText = view.findViewById(R.id.date_text);
 
 
         //calendarView.setDate(20221030);
-        desText.setText(ingredients.get(0).getDescription());
-        locationText.setText(ingredients.get(0).getLocation());
-        CategoryText.setText(ingredients.get(0).getCategory());
-        AmountText.setText(ingredients.get(0).getAmount().toString());
-        UnitText.setText(ingredients.get(0).getUnit());
-        DateText.setText(ingredients.get(0).getBest_before_date().toString());
+        desText.setText(ingredient.getDescription());
+        locationText.setText(ingredient.getLocation());
+        CategoryText.setText(ingredient.getCategory());
+        AmountText.setText(ingredient.getAmount().toString());
+        UnitText.setText(ingredient.getUnit());
+        DateText.setText(ingredient.getBest_before_date().toString());
 
         return view;
     }
