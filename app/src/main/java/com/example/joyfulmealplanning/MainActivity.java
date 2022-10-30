84ae505c2c3168b3f0d84084d017d9ea24d31d5a
Fan Zhu <fzhu@ualberta.ca>
2022-10-30 02:59:09 -0600

    Javadocs added in IngredientController.java

--- app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
+++ app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
@@ -27,13 +27,15 @@ import java.util.Map;
 /**
  * <h1>Joyful Meal Planning</h1>
  *
+ * <b>Credited to:</b> CMPUT301F22T03
+ *
  * <p>
  *     This app is designed to keep track of meal plans of users. It consists of four activities, including ingredients, recipes,
  *     shopping lists and meal plans. Each activity records the entries of ingredients, recipes, shopping lists and meal plans,
  *     users are free to add, edit and delete the entries or even upload images.
  * </p>
  *
- * This activity class implements functionalities to switch to different activities, as well as to manipulate data in the Firestore database
+ * This activity class implements the start page of the app, as well as functionalities to switch to different activities.
  *
  * @author Fan Zhu, Mashiad Hasan, Yuxuan Yang, Xiangxu Meng, Qiaosong Deng & Zhaoqi Ma
  * @version 1.0
@@ -41,7 +43,7 @@ import java.util.Map;
  */
 public class MainActivity extends AppCompatActivity {
 
-
+    /*Declaration of variables*/
     Button Recipe;
     Button MealPlan;
     ImageView imageView;
