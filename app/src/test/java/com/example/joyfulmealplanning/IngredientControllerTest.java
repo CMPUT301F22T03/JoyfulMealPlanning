6c8b027ae429560c2da55482f6a3f81a667fd509
“Mashiad <mashiad@ualberta.ca>
2022-11-01 01:07:43 -0600

    Added IngredientsTest. (Unit Test)

--- app/src/test/java/com/example/joyfulmealplanning/IngredientControllerTest.java
+++ app/src/test/java/com/example/joyfulmealplanning/IngredientControllerTest.java
@@ -6,9 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
-
 import java.util.ArrayList;
 
+
 public class IngredientControllerTest {
+
+
+    private IngredientController ingredientController;
+
+    @Test
+    public void initDBListenerTest() {
+
+
+
+        //ingredientController = new IngredientController(null);
+        //ArrayList<Ingredients> ingredientList = ingredientController.getIngredientList();
+        //assertEquals(ingredientList.size(),1);
+    }
 }
