fa0965ce3257b39834fa2c7f3ac6a351c0cc1fc6
“Mashiad <mashiad@ualberta.ca>
2022-10-31 19:42:01 -0600

    Added IngredientControllerTest and fixed dependencies.

---
+++ app/src/test/java/com/example/joyfulmealplanning/IngredientControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.joyfulmealplanning;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+import java.util.ArrayList;
+
+public class IngredientControllerTest {
+}
