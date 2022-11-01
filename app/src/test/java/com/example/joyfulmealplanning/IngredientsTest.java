55f61ae6f46aca66f8a55883d82a583677cc2b12
“Mashiad <mashiad@ualberta.ca>
2022-11-01 01:18:17 -0600

    Update IngredientsTest.java
    With getters and setters for Description and Best before date.

--- app/src/test/java/com/example/joyfulmealplanning/IngredientsTest.java
+++ app/src/test/java/com/example/joyfulmealplanning/IngredientsTest.java
@@ -12,6 +12,41 @@ import java.util.ArrayList;
 
 public class IngredientsTest {
 
+    private Ingredients ingredients;
 
+    @BeforeEach
+    private void createTest(){
+        ingredients = new Ingredients("apple",20221231,"fridge","fruit",10,"g");
+    }
+
+    @Test
+    public void getDescriptionTest(){
+        String desc = "mango";
+        ingredients = new Ingredients("mango",20221231,"fridge","fruit",10,"g");
+        assertEquals(ingredients.getDescription(), desc);
+    }
+
+    @Test
+    public void setDescriptionTest(){
+        String desc = "apple";
+        ingredients = new Ingredients("mango",20221231,"fridge","fruit",10,"g");
+        ingredients.setDescription(desc);
+        assertEquals(ingredients.getDescription(), desc);
+    }
+
+    @Test
+    public void getBest_before_dateTest(){
+        Integer date = 20221231;
+        ingredients = new Ingredients("mango",20221231,"fridge","fruit",10,"g");
+        assertEquals(ingredients.getBest_before_date(), date);
+    }
+
+    @Test
+    public void setBest_before_dateTest(){
+        Integer date = 20221125;
+        ingredients = new Ingredients("mango",20221231,"fridge","fruit",10,"g");
+        ingredients.setBest_before_date(date);
+        assertEquals(ingredients.getBest_before_date(), date);
+    }
 
 }
