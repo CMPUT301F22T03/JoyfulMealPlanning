98b68cf5e451a509edf8d6908baa313e67b692f6
“Mashiad <mashiad@ualberta.ca>
2022-11-03 02:28:16 -0600

    Added Javadocs for the MealPlan class.

--- app/src/main/java/com/example/joyfulmealplanning/MealPLan.java
+++ app/src/main/java/com/example/joyfulmealplanning/MealPLan.java
@@ -12,10 +12,10 @@ public class MealPLan {
 
     /**
      * This is a constructor for a Meal Plan object.
-     * @param mealPlanID This is the ID for the meal plan {@link String}
-     * @param ID This is the ID for the ingredient or recipe the meal plan is based on {@link String}
-     * @param numberOfServings This is the number of servings for the meal plan {@link Integer}
-     * @param type 
+     * @param mealPlanID The ID for the meal plan {@link String}
+     * @param ID The ID for the ingredient or recipe the meal plan is based on {@link String}
+     * @param numberOfServings The number of servings for the meal plan {@link Integer}
+     * @param type Indicates whether the meal plan is based on recipe or ingredient {@link String}
      */
     public MealPLan(String mealPlanID, String ID, Integer numberOfServings, String type) {
         this.mealPlanID = mealPlanID;
@@ -24,34 +24,66 @@ public class MealPLan {
         this.type = type;
     }
 
+    /**
+     * This method returns the mealPlanID for the meal plan
+     * @return {@link String}
+     */
     public String getMealPlanID() {
         return mealPlanID;
     }
 
+    /**
+     * This method sets a new mealPlanID
+     * @param mealPlanID
+     */
     public void setMealPlanID(String mealPlanID) {
         this.mealPlanID = mealPlanID;
     }
 
+    /**
+     * This method returns the ID for the ingredient or recipe the meal plan is based on
+     * @return {@link String}
+     */
     public String getID() {
         return ID;
     }
 
+    /**
+     * This methods sets a new ID for the ingredient or recipe the meal plan is based on
+     * @param ID {@link String}
+     */
     public void setID(String ID) {
         this.ID = ID;
     }
 
+    /**
+     *This method returns the number of servings for the meal plan
+     * @return {@link Integer}
+     */
     public Integer getNumberOfServings() {
         return numberOfServings;
     }
 
+    /**
+     *This method sets a new number of servings for the meal plan
+     * @param numberOfServings {@link Integer}
+     */
     public void setNumberOfServings(Integer numberOfServings) {
         this.numberOfServings = numberOfServings;
     }
 
+    /**
+     *This methods returns whether the meal plan is based on recipe or ingredient (type of meal plan)
+     * @return {@link String}
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     *This methods sets a new type for the meal plan, to modify whether the meal plan is based on recipe or ingredient
+     * @param type {@link String}
+     */
     public void setType(String type) {
         this.type = type;
     }
