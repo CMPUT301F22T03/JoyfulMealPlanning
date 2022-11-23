b387883b5ddc1e2262b6cad26bc10121a9ea7539
Yuxuan Yang <yang28@ualberta.ca>
2022-11-22 19:02:31 -0700

    Change variable IDs of main activity

--- app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
+++ app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
@@ -1,28 +1,12 @@
 package com.example.joyfulmealplanning;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
 
-import android.widget.Button;
 import android.widget.ImageView;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
@@ -55,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Recipe = findViewById(R.id.imageView3);
+        Recipe = findViewById(R.id.recipeButton);
         Recipe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        MealPlan = findViewById(R.id.imageView4);
+        MealPlan = findViewById(R.id.mealPlanButton);
         MealPlan.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -73,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ShoppingList = findViewById(R.id.imageView2);
+        ShoppingList = findViewById(R.id.shoppingListButton);
         ShoppingList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        imageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.ingredientButton);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
