1d2d2790a2f57613964088ba37c7c024f945ea85
Fan Zhu <fzhu@ualberta.ca>
2022-11-24 22:07:53 -0700

    SignUpActivity layout file edited

--- app/src/main/java/com/example/joyfulmealplanning/SignUpActivity.java
+++ app/src/main/java/com/example/joyfulmealplanning/SignUpActivity.java
@@ -41,6 +41,7 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 createUser();
+                System.out.println(1);
             }
         });
 
@@ -66,6 +67,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         }
         else {
+            System.out.println("Dude please!");
             joyfulMealPlanningAuth.createUserWithEmailAndPassword(email, psw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
