315efb6cef80ed126c196a5e38139311352200ea
Xiangxu Meng <xiangxu@ualberta.ca>
2022-11-25 01:42:34 -0700

    Update LoginActivityTest
    Update LoginActivityTest

---
+++ app/src/androidTest/java/com/example/joyfulmealplanning/LoginActivityTest.java
@@ -0,0 +1,57 @@
+package com.example.joyfulmealplanning;
+
+import android.app.Activity;
+import android.widget.ImageView;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+    /*Declaration of variables*/
+    private Solo solo;
+
+
+    /*Establishes test rules*/
+    @Rule
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<>(LoginActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void a_start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+}
