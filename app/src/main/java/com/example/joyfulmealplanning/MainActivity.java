79e632d8fcf21f4e69f8849daaa442a833b4b97e
Fan Zhu <fzhu@ualberta.ca>
2022-11-24 21:05:57 -0700

    Login functionality viable

--- app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
+++ app/src/main/java/com/example/joyfulmealplanning/MainActivity.java
@@ -10,10 +10,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -52,11 +56,13 @@ public class MainActivity extends AppCompatActivity {
     ImageView ShoppingList;
     FirebaseAuth joyfulMealPlanningAuth;
 
+    //Menu dropdownMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        joyfulMealPlanningAuth = FirebaseAuth.getInstance();
 
         Recipe = findViewById(R.id.imageView3);
         Recipe.setOnClickListener(new View.OnClickListener() {
@@ -95,14 +101,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dropdown_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out:
+                Toast.makeText(this, "Logging out ...", Toast.LENGTH_SHORT);
+                joyfulMealPlanningAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser firebaseUser = joyfulMealPlanningAuth.getCurrentUser();
-
+        if (firebaseUser == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 
     /**
